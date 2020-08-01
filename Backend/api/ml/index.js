3b9b1e5c001319b5fe2efecbd7472ef4f31195b5
satya9500 <satya.prakash9500@gmail.com>
2020-08-01 16:15:37 +0530

    video progress

--- Backend/api/ml/index.js
+++ Backend/api/ml/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const {ocr, summary, translate} = require('./controller');
+const {ocr, summary, translate, imageSearch, textToSpeech, wordpos, video} = require('./controller');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 router.route('/ocr').post(ocr);
 router.route('/summary').post(summary);
 router.route('/translate').post(translate);
+router.route('/image').post(imageSearch);
+router.route('/speech').post(textToSpeech);
+router.route('/wordpos').post(wordpos);
+router.route('/video').post(video);
+
 module.exports = router;
